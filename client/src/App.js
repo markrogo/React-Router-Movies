0be4963b26678a3cab55e6dfeee93fe0fd6c2029
Mark Rogowsky <65688548+markrogo@users.noreply.github.com>
2020-09-02 02:21:19 -0700

    the basic movielist is working, but why???

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Link, Route, Switch, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -13,6 +16,8 @@ const App = () => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovieList(response.data);
+          console.log(response.data);
+          console.log (movieList); // please explain why this doesn't work?!?
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -28,9 +33,22 @@ const App = () => {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-      <div>Replace this Div with your Routes</div>
+      
+      <Switch>
+
+         <Route exact path="/" render={(props) => {
+          return <MovieList movies={movieList} />
+        }} /> 
+    
+        <Route path="/movies/:id" component={Movie}>
+           </Route>
+
+      </Switch>
     </div>
   );
 };
 
 export default App;
+
+// one route for / that loads the MovieList component. This component will need the movies injected into it via props.
+//  one route that will take an id parameter after/movies/ (ex: /movies/2, /movies/3 where the id is dynamic). This route should load the Movie component.
